cbb47e9a66a8082930ae50fb24b9d5235d5805fd
Dior.Shelton <dior.shelton@gmail.com>
2024-11-23 23:16:44 -0500

    refactor to return Hello World by default when no name is passed as arg

--- hello_test.go
+++ hello_test.go
@@ -1,11 +1,24 @@
 package main
+
 import "testing"
 
-func TestHello (t *testing.T) {
-	got:= Hello("Chris")
-	want:= "Hello, Chris"
+func TestHello(t *testing.T) {
+	t.Run("saying hellog to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-	if got != want { 
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
