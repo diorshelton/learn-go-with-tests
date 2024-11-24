11a6c321e2aee169fa8d5411c284a8c9b3ea5d55
Dior.Shelton <dior.shelton@gmail.com>
2024-11-23 23:45:56 -0500

    add tests for spanish and french language

--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hellog to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func (t *testing.T)  {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francois", "French")
+		want := "Bonjour, Francois"
 		assertCorrectMessage(t, got, want)
 	})
 }
